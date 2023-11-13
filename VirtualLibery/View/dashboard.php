59f7c2bc541a333dc1e31cc26c0e93ee1df78063
maxweeel <129960146+maxweeel@users.noreply.github.com>
2023-11-13 12:49:23 -0300

    Add files via upload

---
+++ VirtualLibery/View/dashboard.php
@@ -0,0 +1,47 @@
+<?php require_once "../Controller/Auth_Verify.php"; ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-vindo!</title>
+    <link rel="stylesheet" href="dashboard.css"> 
+    
+</head>
+
+<body>
+<nav class="d-flex justify-content-center">
+<div>
+        <h2>VirtualLibrary</h2>
+        <img src="logo.png" alt="logo">
+        <h1>Bem-vindo de volta</h1>
+</div>
+</nav>
+
+<nav class="d-flex justify-content-center">
+<div id="Menu"  class="sidenav">
+        <a href="add-new-call.html" 
+
+         class="text-sm-center">Adicionar livros</a>
+
+        <a href=""  
+        class="text-sm-center">Editar livros</a>
+
+        <a href=""  
+        class="text-sm-center">Excluir livros</a>
+
+        <a href=""  
+        class="text-sm-center">Visualizar perfil</a>
+
+        <a href="index.html">Sair</a>
+</div>
+</nav>
+
+       <p>Opções de leitura:</p>
+<div class="container">
+       <h4><b>Um Titulo Qualquer</b></h4>
+       <button  href="">Ler</button>
+</div>
+</body>
+</html>
